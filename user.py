486fd704ab74becf4405e023f2d8890d4c867a14
monginadiana <diana.ondieki@student.moringa.com>
2021-10-23 20:45:08 +0300

    creation of the user class and the function that allows users to save users

---
+++ user.py
@@ -0,0 +1,27 @@
+from _typeshed import Self
+
+
+class User:
+    """
+    creation of a new user class instance
+    """
+    users_list = []
+
+    def __init__(self, username, password):
+
+        '''
+        __init__ method that helps us define the users objects properties
+
+        Args:
+            username: login's username for a new user
+            password: login:s password for a new user
+        '''
+    
+        self.username = username
+        self.password = password
+
+    def save_user (self):
+      """
+      A function that allows users to save their passwords
+      """
+      User.users_list.append(self)
