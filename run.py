91f64b9f318e34a22ec2ea52138bd6121d324478
monginadiana <diana.ondieki@student.moringa.com>
2021-10-24 01:25:01 +0300

    creating a function for new user

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ from user import User
 from account import Credentials
 import random
 import string
+
+def new_person (username,password):
+    '''
+    This is a function that creates a new user
+    '''
+    new_user = User(username,password)
+    return new_user
