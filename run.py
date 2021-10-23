5551816a66c53a245e0a45d572bae43cf813ff1f
monginadiana <diana.ondieki@student.moringa.com>
2021-10-24 02:23:05 +0300

    corection of eerror in the account

--- run.py
+++ run.py
@@ -5,16 +5,16 @@ import random
 import string
 
 def new_person (username,password):
-    '''
+    """
     This is a function that creates a new user
-    '''
+    """
     new_user = User(username,password)
     return new_user
 
 def save_new_person(user):
-    '''
+    """
     Function to save the new person
-    '''
+    """
     user.save_user()
 def delete_new_person(user):
     """
@@ -28,33 +28,32 @@ def find_new_person(user):
     user.find_username()
 
 def create_new_account(account_name,username,password):
-    '''
+    """
     Function to create a new account
-    '''
+    """
     new_account= Credentials(account_name,username,password)
     return new_account
 
 def save_social_account(account):
-    '''
+    """
     function to save a social account's credentials
-    '''
+    """
     account.save_account()
 
 def delete_social_account(account):
-    '''
-    function to delete a saved social accounts
-    '''
-    account.delete_account()  
+   """
+   function to delete a saved social accounts
+   """
+   account.delete_account()  
 def display_accounts():
         """
         test method that returns a list of all accounts created in our list"
         """
         return Credentials.display_accounts()
 def create_password(length=6):
-
-    '''
-    function that generates a password for you
-    '''
+    """
+    A funtion that allows users to be able to generate new password
+    """
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(characters) for i in range(length))
 
