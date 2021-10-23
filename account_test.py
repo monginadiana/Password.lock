30faf092be50b1eef96631c88603135d8f504dc9
monginadiana <diana.ondieki@student.moringa.com>
2021-10-24 00:15:16 +0300

    importing of the credential account

---
+++ account_test.py
@@ -0,0 +1,12 @@
+import unittest
+from account import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    
