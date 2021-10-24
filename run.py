c51c94c6e9f002d71e87f96e8b183a64600a8c79
monginadiana <diana.ondieki@student.moringa.com>
2021-10-24 16:50:40 +0300

    addition of the log in creation cheat code

--- run.py
+++ run.py
@@ -99,17 +99,17 @@ def main():
             login_password = input()
             if username != login_username or password != login_password:
                 print('Invalid Username or password!')
-                print('Re-enter your username....')
+                print('Please enter your password....')
                 login_username = input()
 
-                print('Re- enter your password....')
+                print('Please enter your password...')
                 login_password = input()
 
             else :
                     print ('\n')
                     print(f'Hello {login_username}. Welcome to your password locker account!')
                     print ('\n')
-                    save_social_account()
+                    social_account()
         elif short_code == 'lg':
              
                 print('Log in to your existing account')
@@ -119,158 +119,25 @@ def main():
                 print('Password....')
                 lg_password = input()
 
-                if lg_username != 'Felista' and lg_password != 'Felista1#':
+                if lg_username != 'Diana' and lg_password != '@Mongina':
                     print('The account does not exist, please create an account')
                 else:
-                    print(f'Hello {lg_username}. Welcome to your password locker account!')
+                    print(f'Hello {lg_username}. This is your password locker account, Welcome!')
                     print ('\n')
-                    save_social_account()
+                    social_account()
         elif short_code == 'ex':
 
-                print('Bye! You can come back later!')
+                print('Bye! Come back soon !')
                 break
         else:
                 print('Wrong short code! Try again')
-        
-                 
 
 
-
-
-
-
-
-
-
-
-
-
-
-#             if username != login_username or password != login_password:
-#                 print('Invalid Username or password!')
-#                 print('Re-enter your username....')
-#                 login_username = input()
-
-#                 print('Re- enter your password....')
-#                 login_password = input()
-
-#             else :
-#                  print ('\n')
-#                  print(f'Hello {login_username}. Welcome to your password locker account!')
-#                  print ('\n')
-#                  social_account()
-
-#         elif short_code == 'lg':
-             
-#                 print('Log in to your existing account')
-#                 print('Username....')
-#                 lg_username = input()
-
-#                 print('Password....')
-#                 lg_password = input()
-
-#                 if lg_username != 'Felista' and lg_password != 'Felista1#':
-#                     print('The account does not exist, please create an account')
-#                 else:
-#                     print(f'Hello {lg_username}. Welcome to your password locker account!')
-#                     print ('\n')
-#                     social_account()
-#         elif short_code == 'ex':
-
-#                 print('Bye! You can come back later!')
-#                 break
-#         else:
-#                 print('Wrong short code! Try again')
-
-
-# def social_account():
-#     print('Now you can store your social accounts credentials here!')
+def social_account():
+    print('Now you can store your social accounts credentials here!')
   
-#     print('Use these short codes to navigate through')
-#     print ('\n')
-
-#     while True:
-#             print('asc - Add a new social account credentials')
-#             print('esc - Add an existing social account credentials')
-#             print('dsc - Display your available social accounts credentials')
-#             print('del - Delete a saved account credentials')
-#             print('ex - Exit from the account')
-
-#             short_code = input().lower()
-
-#             if short_code == 'asc':
-#                 print('Add a new social account credentials')
-#                 print("-"*10)
-
-#                 print('Social Account Name ...')
-#                 accountname = input()
-
-#                 print('Username....')
-#                 username= input()
-                
-#                 print ('\n')
-#                 print('Use:')
-#                 print('gp - To get the password generated for you')
-#                 print('cp - To create your own password')
-
-#                 short_code1 = input().lower()
-#                 if short_code1 == 'gp':
-#                     password = create_password()
-#                     print(password)
-#                 elif short_code1 == 'cp':
-#                     print('Password....')
-#                     password = input()
-#                 else:
-#                     print('Invalid short code')      
-
-#                 save_social_account(create_new_account(accountname,username,password))
-                   
-#                 # print ('\n')
-#                 print(f'{accountname} account credentials have been saved and stored')
-            
-#             elif short_code == 'esc':
-#                 print('Add your existing account credentials')
-#                 print("-"*10)
-
-#                 print('Social Account Name ...')
-#                 accountname = input()
-
-#                 print('Username...')
-#                 username = input()
-
-#                 print('Password...')
-#                 password = input()
-
-#                 save_social_account(create_new_account(accountname,username,password))
-
-#                 print(f'{accountname} account credentials have been saved and stored')
-                
-#             elif short_code == 'dsc':
-#                 if display_accounts():
-#                     print("This is a list of all your saved account credentials") 
-#                     print("-"*20)   
-
-#                     for social in display_accounts():
-#                         print(
-#                             f"{social.account_name} {social.username} {social.password}"
-#                         )
-#                     print('\n')    
-#                 else:
-#                     print('\n')
-#                     print('You do not have any social account credentials saved')       
-#                     print('\n')
-
-#             elif short_code == 'del':
-#                 print('Which account do you want to delete?')
-
-#                 deleted_account = input()       
-                 
-#                 delete_social_account(deleted_account)
-
-#             elif short_code == 'ex':
-#                 print('Bye! You can come back later.')    
-#             else:
-#                 print('Wrong short code! Try again')
+    print('Use these short codes to navigate through')
+    print ('\n')
 
 if __name__ == '__main__':
 
